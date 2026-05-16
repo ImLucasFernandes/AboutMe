bed87c7445b4a0e25b7b2786d7b5b30da8e0afc8
Lucas Fernandes <lucasisclueless@gmail.com>
2026-05-16 16:23:08 +0000

    Unformatted Pre-Database Danguage Explanation Integration

--- responsive.js
+++ responsive.js
@@ -25,6 +25,22 @@ for (let i = 0; i < sectionList.length; i++) {
     })
     //currently have no experience with database development but convert to get/pull request after
     sectionList[i].addEventListener("click", function () { 
-        moreInfo.innerHTML = "More info on: " + sectionList[i].textContent;
+        moreInfo.innerHTML = "More info on: " + sectionList[i].textContent + explanations[sectionList[i].id];
     })
+}
+//prebuilt list of longer explanations before php/data base implementation
+let explanations = {
+    webDesign : " This website is an exmaple of the experience " +
+    "i've gained in web development which includes html structures, vanilla css and bootstrap, " +
+    "DOM manipulation, AJAX, svg graphics and animations, responsive design and dynamic elements", 
+    
+    OOP : " Object Oriented Programming was taught with the use of JAVA and covered" +
+        "Encapsulation, AssociaHTML & CSStion, Inheritance, Polymorphism and Memory Leaks. ", 
+    
+    cSharp : " I gained experience with C sharp in highschool which covered everything from" + 
+            " sorting + searching methods, in depth GUI manipulation, program logic (games + calculator) and culminated in a " + 
+            "top down doom inspired platformer. ", 
+    
+    python : " I learned Python in highschool and first simester fundamentals which covered basic canvas manipulation and animations. basic syntax and logic and culminated in"
+    + "a 2d sidescroller project, during fundamentals theres also the project gutenberg terminal search program homebrew within the portfolio list. "
 }
