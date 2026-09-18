7f3aed42bf813d64149c81ab19fe1272af62022d
Lucas Fernandes <lucasisclueless@gmail.com>
2026-09-18 17:53:53 +0000

    button animation

--- responsive.js
+++ responsive.js
@@ -18,10 +18,10 @@ let explanationSection = document.querySelector("#explanationSect");
 let moreInfo = document.querySelector("#moreInfo");
 for (let i = 0; i < sectionList.length; i++) {
     sectionList[i].addEventListener("mouseover", function () { 
-        sectionList[i].style.textDecoration = "underline";      
+        sectionList[i].style.border = "1px, solid, black";      
     })
     sectionList[i].addEventListener("mouseout", function () { 
-        sectionList[i].style.textDecoration = "none";      
+        sectionList[i].style.textDecoration.border = "none";      
     })
     //currently have no experience with database development but convert to get/pull request after
     sectionList[i].addEventListener("click", function () { 
